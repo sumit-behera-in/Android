3c613e4188f994ecc1191a05510cef2872ebde2f
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2023-06-02 01:22:06 +0530

    Created

--- task/app/src/main/java/com/example/task/MainActivity.kt
+++ task/app/src/main/java/com/example/task/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.task
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.task.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private val mycalender = Calendar.getInstance()
+    private val datePicker = DatePickerDialog.OnDateSetListener{View,year,month,dayofmonth ->
+        mycalender.set(Calendar.YEAR,year)
+        mycalender.set(Calendar.MONTH,month)
+        mycalender.set(Calendar.DAY_OF_MONTH,dayofmonth)
+        updateLabel(mycalender)
+    }
+
+    private fun updateLabel(mycalender: Calendar) {
+        val myformat = "dd-mm-yyyy"
+        val sdf = SimpleDateFormat(myformat, Locale.UK)
+        binding.editDOB.setText(sdf.format(mycalender.time))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        //below line is very very important
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.string=Mystrings()
+        //above line is very very important
+
+
+        binding.editDOB.setOnClickListener{
+                DatePickerDialog(this,datePicker,mycalender.get(Calendar.YEAR),mycalender.get(Calendar.MONTH),mycalender.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+        binding.submit.setOnClickListener {
+            binding.heading.text = "You have sucessfully Registered"
+
+            binding.txtName.text = binding.editName.text
+            binding.txtName.visibility= View.VISIBLE
+            binding.editName.visibility=View.GONE
+
+            binding.txtDOB.text=binding.editDOB.text
+            binding.txtDOB.visibility=View.VISIBLE
+            binding.editDOB.visibility=View.GONE
+
+            binding.txtMobile.text=binding.editMobile.text
+            binding.txtMobile.visibility=View.VISIBLE
+            binding.editMobile.visibility=View.GONE
+
+            Toast.makeText(applicationContext,"Competed",Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- task/app/src/main/java/com/example/task/Mystrings.kt
+++ task/app/src/main/java/com/example/task/Mystrings.kt
@@ -1,3 +1,3 @@
 package com.example.task
 
-data class Mystrings(var up:String="Registration Form")
+data class Mystrings(var up:String="Registration Form",var name:String=" ",var dob:String=" ",var mob:String=" ",var submit:String="SUBMIT")
