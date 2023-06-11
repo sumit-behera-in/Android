ec9e52e209c3556f85a64822ba9479e235742b03
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2023-06-12 00:02:53 +0530

    about

--- NavProj1_Quiz_app/app/src/main/java/com/example/navproj1/Title.kt
+++ NavProj1_Quiz_app/app/src/main/java/com/example/navproj1/Title.kt
@@ -3,10 +3,15 @@ package com.example.navproj1
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class Title : Fragment() {
 
@@ -21,7 +26,17 @@ class Title : Fragment() {
             val action = TitleDirections.actionTitleToQ1()
             Navigation.findNavController(view).navigate(action)
         }
+        setHasOptionsMenu(true)
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.about_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())||super.onOptionsItemSelected(item)
     }
 
 }
